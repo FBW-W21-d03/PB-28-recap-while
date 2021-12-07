2508e9b2a2a2fb544d9fe7c8f2c9d1ddc96b8d3a
ShiinyFlakes <mago_elm@hotmail.com>
2021-12-07 09:32:03 +0100

    Lösung

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var hilfsvariable=2;
 while (ungerade.length<=1000) {
 
     ungerade.push(hilfsvariable+1);
-    hilfsvariable+=2;//varhilf=varhilf+2
+    hilfsvariable+=2;
 
 };
 console.log(ungerade);
