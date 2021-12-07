6aec2b0215a6e4833df27cb484503e0dc3b3ec5f
H94ssein44 <alali.52@yahoo.com>
2021-12-07 10:34:32 +0100

    done

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+let ungrade = [];
+let zahl = 0;
+while (zahl <= 1000) {
+    zahl++
+    console.log(zahl)
+    if (zahl % 2 != 0) {
+        ungrade.push(zahl)
+    }
+}
+console.log(ungrade)
