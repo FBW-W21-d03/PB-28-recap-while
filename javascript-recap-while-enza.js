1b84656679893313569011e562bd1bbf574fe7db
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2021-12-07 10:03:11 +0100

    enza

---
+++ javascript-recap-while-enza.js
@@ -0,0 +1,14 @@
+
+let oddNumbers = [];
+
+let currentNumber = 0;
+
+while ((oddNumbers.length <= 1000)) {
+        oddNumbers.push(currentNumber + 1);
+    currentNumber += 2; 
+    
+}
+
+console.log(oddNumbers);
+console.log("-------------------");
+
