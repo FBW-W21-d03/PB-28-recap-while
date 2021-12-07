d0d0b09994a94665fda5a06c92d33ea1d1a84192
ShiinyFlakes <mago_elm@hotmail.com>
2021-12-07 09:30:43 +0100

    Lösung

---
+++ script.js
@@ -0,0 +1,12 @@
+var ungerade=[];
+var hilfsvariable=2;
+
+while (ungerade.length<=1000) {
+
+    ungerade.push(hilfsvariable+1);
+    hilfsvariable+=2;//varhilf=varhilf+2
+
+};
+console.log(ungerade);
+
+
