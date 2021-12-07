326d924385e1f9aaef106bff047f60db83ca932c
sprotte <metatron1972@live.de>
2021-12-07 10:09:59 +0100

    sprotte

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-
+/*
 let grade = [];
 let ungrade = [];
 let ausgabe = 0;
 
-while ((grade.length<=100) && (ungrade.length<=100)) {
+while ((grade.length<=1000) && (ungrade.length<=1000)) {
     grade.push(ausgabe);
-    ungrade.push(ausgabe+1);
+    ungrade.push(ausgabe+2);
     ausgabe+=1;
-    if((grade.length == 100) && (ungrade.length == 100)){
+    if((grade.length == 1000) && (ungrade.length == 1000)){
         console.log(grade, ungrade);
     }
         }
+*/
+/*Anderer Weg*/
+let ungeradeZahlen = -1;
+const ungeradeZahlenSrr = [];
+
+while (ungeradeZahlen < 1000) {
+    ungeradeZahlen += 1;
+    ungeradeZahlenSrr.push(ungeradeZahlen);
+}
+
+console.log(ungeradeZahlenSrr);
