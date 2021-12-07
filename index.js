b6d49f627ee7469b4f771198b3447e0c47246eeb
aisha2000-hub <aisha.noori2000@gmail.com>
2021-12-07 10:18:59 +0100

    Die Lösung von Aisha

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let ungeradeZahlen = [];
+let num = 0;
+
+while(num <= 1000){
+
+    if(num % 2  != 0){
+
+        ungeradeZahlen.push(num)
+    }
+    num++;
+   
+}
+
+console.log(ungeradeZahlen)
+
