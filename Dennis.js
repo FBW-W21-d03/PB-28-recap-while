a9c59ff83767f4296a389bd69c867f531d0e1052
Dennisbullys <d.rochus@hotmail.de>
2021-12-07 10:13:20 +0100

    DennisNeueLösung

--- Dennis.js
+++ Dennis.js
@@ -2,12 +2,12 @@
 
 const ungrade=[]
 
-let number=0
+let number= 0
 
-while (ungrade.length<=1000)
+while (ungrade.length<=1001)
 {
   
-  ungrade.push(number + 0);
-  number+=3;
+  ungrade.push(number + 1);
+  number+=2;
 };
 console.log(ungrade);
