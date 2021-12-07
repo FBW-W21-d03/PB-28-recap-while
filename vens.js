5b2f098878387fa772e58bc5de8ea2df2fda8de8
Vens <v.kolev@alfatrex.com>
2021-12-07 09:57:10 +0100

    vens

---
+++ vens.js
@@ -0,0 +1,16 @@
+let geradeZahl = -2;
+const GeradeZahl = [];
+
+while (geradeZahl < 1000) {
+    console.log(geradeZahl += 2);
+    GeradeZahl.push(geradeZahl);
+}
+
+
+let ungeradeZahl = -1;
+const unGeradeZahl = [];
+
+while (ungeradeZahl < 1000) {
+    console.log(ungeradeZahl += 2);
+    unGeradeZahl.push(ungeradeZahl);
+}
